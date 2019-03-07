03a1ba96b0d5475e172944d753fbf2021cd53a73
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-03-07 13:41:33 +0000

    Refactor renamed test for clarity.

--- src/test/java/bowlingkata/GameShould.java
+++ src/test/java/bowlingkata/GameShould.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GameShould {
 
     @Test
-    void a_game_starts_with_zero_points() {
+    void start_with_zero_points() {
         Game game = new Game();
         assertEquals(0, game.score());
     }
