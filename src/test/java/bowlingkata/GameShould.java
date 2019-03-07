2addf91a1bba44f153bc82099cd7ba6c62bc0dfe
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-03-07 13:47:13 +0000

    Add test setup method and renamed test.

--- src/test/java/bowlingkata/GameShould.java
+++ src/test/java/bowlingkata/GameShould.java
@@ -1,24 +1,36 @@
 package bowlingkata;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameShould {
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
 
     @Test
     void start_with_zero_points() {
-        Game game = new Game();
         assertEquals(0, game.score());
     }
 
     @Test
     void add_score_has_player_roll() {
-        Game game = new Game();
-
         game.roll(1);
 
         assertEquals(1, game.score());
+    }
+
+    @Test
+    void sum_rolls() {
+        game.roll(1);
+        game.roll(1);
+
+        assertEquals(2, game.score());
     }
 }
